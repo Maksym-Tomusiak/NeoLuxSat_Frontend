0897af974bc8347d3255ce68c73151507ba5a112
Maksym-Tomusiak <maxtomusiak315@gmail.com>
2025-11-08 14:17:32 +0200

    fix: fix repairs search and pagination for many  pages

--- neoluxsat_frontend/src/services/textService.ts
+++ neoluxsat_frontend/src/services/textService.ts
@@ -1,27 +1,47 @@
-import type { PaginationParams } from '@/types/paginationParams';
+import type {
+  PaginationParams,
+  RepairPaginationParams,
+} from '@/types/paginationParams';
 
 export class TextService {
   public static truncate(text: string, length: number): string {
-    if (text.length > length) {
-      return text.substring(0, length) + '...';
-    }
-    return text;
+    return text.length > length ? text.substring(0, length) + '...' : text;
+  }
+
+  private static basePaginationToText(
+    pagination: PaginationParams
+  ): URLSearchParams {
+    const queryParams = new URLSearchParams();
+
+    const { pageNumber, pageSize, searchTerm, sortBy, sortDescending } =
+      pagination;
+
+    if (pageNumber != null)
+      queryParams.append('pageNumber', pageNumber.toString());
+    if (pageSize != null) queryParams.append('pageSize', pageSize.toString());
+    if (searchTerm) queryParams.append('searchTerm', searchTerm);
+    if (sortBy) queryParams.append('sortBy', sortBy);
+    if (sortDescending != null)
+      queryParams.append('sortDescending', sortDescending.toString());
+
+    return queryParams;
   }
 
   public static paginationToText(pagination: PaginationParams): string {
-    const queryParams = new URLSearchParams();
-    if (pagination.pageNumber != null)
-      queryParams.append('pageNumber', pagination.pageNumber.toString());
-    if (pagination.pageSize != null)
-      queryParams.append('pageSize', pagination.pageSize.toString());
-    if (pagination.searchTerm)
-      queryParams.append('searchTerm', pagination.searchTerm);
-    if (pagination.sortBy) queryParams.append('sortBy', pagination.sortBy);
-    if (pagination.sortDescending != null)
-      queryParams.append(
-        'sortDescending',
-        pagination.sortDescending.toString()
-      );
+    const queryParams = this.basePaginationToText(pagination);
+    return `?${queryParams.toString()}`;
+  }
+
+  public static repairPaginationToText(
+    pagination: RepairPaginationParams
+  ): string {
+    const queryParams = this.basePaginationToText(pagination);
+
+    if (pagination.statusId)
+      queryParams.append('statusId', pagination.statusId);
+    if (pagination.paymentId)
+      queryParams.append('paymentId', pagination.paymentId);
+
     return `?${queryParams.toString()}`;
   }
 }
